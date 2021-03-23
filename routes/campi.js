1fe06d7a3b45927ad47c6300cbcfa0ea890fa7b6
Simone La Falce <matteo.brienza@nttdata.com>
2021-03-23 14:00:49 +0100

    First Commit

---
+++ routes/campi.js
@@ -0,0 +1,19 @@
+'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET campi listing. */
+router.get('/', function (req, res) {
+    var campi = [{
+        nome: "Campo1",
+        codice: "AAA"
+    }, {
+        nome: "Campo2",
+        codice: "AAB"
+    }]
+
+    console.log("DEBUG")
+    res.json(campi);
+});
+
+module.exports = router;
